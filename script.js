89b0a4b05d00df190ced8ac29c833c3b9f0df7f9
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2022-09-14 19:28:35 -0300

    Add SketchGrid and button for change grid size

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const gridContainer = document.querySelector('.grid-container');
 
-console.log(gridContainer);
+//
+function generateGrid(size){
+    if (size > 100 || isNaN(size) || size <= 0){
+        alert("Enter a valid number");
+        return;
+    }
 
-for (let i = 0; i < 256; i++){
-    let gridPixel = document.createElement("div")
-    gridContainer.appendChild(gridPixel);
+    deleteGrid();
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size*size; i++){
+        const gridPixel = document.createElement("div");
+        gridContainer.appendChild(gridPixel);
+    }
+
+    const gridPixels = gridContainer.querySelectorAll('div');
+    gridPixels.forEach(pixel => pixel.addEventListener('mouseenter', () => pixel.classList.add('colored')));
 }
+
+function deleteGrid() {
+    gridContainer.innerHTML='';
+}
+
+const gridButton = document.querySelector('.grid-button').addEventListener('click', () => generateGrid(prompt('Enter the grid size: (1-100)')))
+
+
+function main() {
+    deleteGrid();
+    generateGrid(16);
+}
+
+main();
