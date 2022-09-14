800d388adc23b1e24dec6b6d31b28ef7e3f9eedb
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2022-09-14 20:26:04 -0300

    Add rainbow and pencil option

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function get_random_rgb_color() {
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    return "rgb(" + x + "," + y + "," + z + ")";
+}
+
 const gridContainer = document.querySelector('.grid-container');
 
 //
@@ -16,11 +23,30 @@ function generateGrid(size){
     }
 
     const gridPixels = gridContainer.querySelectorAll('div');
-    gridPixels.forEach(pixel => pixel.addEventListener('mouseenter', () => pixel.classList.add('colored')));
+    gridPixels.forEach(pixel => pixel.addEventListener('mouseenter',() => coloringPixel(pixel)));
 }
 
 function deleteGrid() {
     gridContainer.innerHTML='';
+}
+
+function coloringPixel(e){
+    const radioButtons = document.querySelectorAll('input[name="check"]')
+    console.log(radioButtons);
+    
+    if (radioButtons[1].checked) {
+        e.style.backgroundColor = get_random_rgb_color();
+    } else if (radioButtons[2].checked) {
+        if (e.style.backgroundColor === 'black'){
+            e.style.opacity = parseFloat(e.style.opacity) + 0.1;
+        } else {
+            e.style.backgroundColor = 'black';
+            e.style.opacity = 0.1;
+        }
+    } else {
+        e.style.backgroundColor = 'black';
+        e.style.opacity = 1
+    }
 }
 
 const gridButton = document.querySelector('.grid-button').addEventListener('click', () => generateGrid(prompt('Enter the grid size: (1-100)')))
